f1864dc009b2883e94d3ce9f04cbc6bbe58fc744
Brian Olsen <brian@maven-group.org>
2014-05-19 11:08:46 +0200

    Run status in its own shell.

--- lib/dur.sh
+++ lib/dur.sh
@@ -20,23 +20,26 @@ function dur {
       else
         home="$HOME";
     fi
-    if git --git-dir="$home/.cfg/.git" check -q; then
-      branch_orig_hash="$(git show-ref -s --verify refs/heads/master 2> /dev/null)"
-      git --git-dir="$home/.cfg/.git" fetch -q origin master
-      branch_remote_hash="$(git show-ref -s --verify refs/remotes/origin/master 2> /dev/null)"
-      if [ "$branch_orig_hash" != "$branch_remote_hash" ]; then
-        if ! git --git-dir="$home/.cfg/.git" pull --ff-only -q origin master 2> /dev/null; then
-          echo ".cfg could not be fast-forwarded"
+    (
+      export GIT_DIR="$home/.cfg/.git"
+      if git check -q; then
+        branch_orig_hash="$(git show-ref -s --verify refs/heads/master 2> /dev/null)"
+        git fetch -q origin master
+        branch_remote_hash="$(git show-ref -s --verify refs/remotes/origin/master 2> /dev/null)"
+        if [ "$branch_orig_hash" != "$branch_remote_hash" ]; then
+          if ! git pull --ff-only -q origin master 2> /dev/null; then
+            echo ".cfg could not be fast-forwarded"
+          fi
         fi
+        branch_hash="$(git show-ref -s --verify refs/heads/master 2> /dev/null)"
+        if [ "$branch_orig_hash" != "$branch_hash" ]; then
+          echo ".cfg has been updated. Reinstalling..."
+          $home/.cfg/install.sh
+        fi
+      else
+        echo ".cfg has uncommitted changes"
       fi
-      branch_hash="$(git show-ref -s --verify refs/heads/master 2> /dev/null)"
-      if [ "$branch_orig_hash" != "$branch_hash" ]; then
-        echo ".cfg has been updated. Reinstalling..."
-        $home/.cfg/install.sh
-      fi
-    else
-      echo ".cfg has uncommitted changes"
-    fi
+    )
     ;;
   move|mv)
     git remote add bitbucket git@bitbucket.org:durdn/$(basename $(pwd)).git
